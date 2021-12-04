3b9efd29b3a129ab6a650752c358dae88b7f1091
NAGENDRA CHINNAM <nagendra571@gmail.com>
2021-12-04 13:45:34 -0500

    Implemented RabitMq Checkout/CreateOrder model.

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -27,14 +28,24 @@ namespace Basket.API
                 m.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
 
-
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddAutoMapper(typeof(Startup));
 
             services
                 .AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
                  m => m.Address = new System.Uri(Configuration["GrpcSettings:DiscountUrl"])
                  );
             services.AddScoped<DiscountGrpcService>();
+
+            // MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+            services.AddMassTransitHostedService();
 
 
             services.AddControllers();
