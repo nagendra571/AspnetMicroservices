adc13d6ca19fc90659efcf2aec0abf53c1c8b990
NAGENDRA CHINNAM <nagendra571@gmail.com>
2021-12-05 17:44:15 -0500

    Implemented shoppingcart.aggregator.

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketItemExtendedModel
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+
+
+        //Product Related Additional Fields
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+    }
+}
